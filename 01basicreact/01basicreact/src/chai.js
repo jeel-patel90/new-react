97d68367f7c554b67c9f676f323bda80a6403041
Jeel Bhalu <75306161+jeel-patel90@users.noreply.github.com>
2024-12-12 14:16:06 +0530

    Removed unused files

---
+++ 01basicreact/01basicreact/src/chai.js
@@ -0,0 +1,7 @@
+function Chai(){
+    return(
+        <h1>Hey! lets create chai with jeelb with the help of react</h1>
+    )
+}
+
+export default Chai
---
+++ 01basicreact/01vitereact/src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+// Remove this line if index.css is deleted
+// import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <App />
+)
+
+
